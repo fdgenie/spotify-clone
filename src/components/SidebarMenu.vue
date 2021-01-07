931de6a97f31be654a8b525f8d8d2f224d8651c1
fdgenie <pa.aris@yahoo.gr>
2021-01-07 15:30:58 +0200

    update ts, fix pages

---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- <aside class="relative h-full w-72 bg-black text-white text-left"> -->
+  <div class="absolute inset-y-0 left-0 w-11/12 pl-4">
+    <div class="pt-6">
+      LOGO
+    </div>
+    <div class="pt-8">
+      <div>
+        <router-link to="/">Home</router-link>
+      </div>
+      <div class="pt-4">
+        <router-link to="/">Search</router-link>
+      </div>
+      <div class="pt-4">
+        <router-link to="/">Library</router-link>
+      </div>
+    </div>
+    <div class="pt-8">
+      Playlist
+      <div>
+        Create Playlist
+      </div>
+      <div>
+        Songs that you like
+      </div>
+    </div>
+    <hr class="border-gray-700 mt-4" />
+    <div class="pt-2" v-for="playlist in playlists" :key="playlist.id">
+      {{ playlist.title }}
+    </div>
+  </div>
+  <!-- </aside> -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SidebarMenu",
+  setup() {
+    const playlists = [
+      {
+        id: 1,
+        title: "playlist1"
+      },
+      {
+        id: 2,
+        title: "playlist2"
+      },
+      {
+        id: 3,
+        title: "playlist4"
+      },
+      {
+        id: 4,
+        title: "playlist4"
+      },
+      {
+        id: 5,
+        title: "playlist5"
+      },
+      {
+        id: 6,
+        title: "playlist6"
+      }
+    ];
+
+    return { playlists };
+  }
+});
+</script>
---
+++ src/views/MainScreenPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-row overflow-hidden">
+    <aside class="relative h-auto w-1/6 bg-black text-white text-left">
+      <SidebarMenu />
+    </aside>
+    <div class="w-5/6 h-5/6">
+      test
+    </div>
+    <div class="absolute inset-x-0 bottom-0 h-auto bg-gray-700">
+      <PlaylistFooter />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import SidebarMenu from "@/components/SidebarMenu.vue";
+import PlaylistFooter from "@/components/PlaylistFooter.vue";
+
+export default defineComponent({
+  name: "MainScreenPage",
+  components: {
+    SidebarMenu,
+    PlaylistFooter
+  }
+});
+</script>
